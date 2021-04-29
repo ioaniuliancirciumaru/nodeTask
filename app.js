8638c0efcba5bd3081976750939a83107ffedc4e
nonu <nonu@nonus-MacBook-Air.local>
2021-04-29 14:16:20 +0300

    finished A and B tasks

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
+const multer = require('multer');
 const express = require('express');
-
+const csvtojson = require('csvtojson');
+const { json } = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const csvFilePath = "people.csv";
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('We are on home');
+
+const upload = multer({ dest: 'tmp/csv/' });
+
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false}));
+
+
+// Task A
+app.post('/insertDoctor', (req, res) => {
+    const doctor = req.body;
+    if (!doctor.active) {
+        console.log("not found");
+    } else if (!doctor.id || doctor.resourceType != "Practitioner") {
+        throw 'Invalid request!';
+    } else {
+        console.log("Doctor's name is: " + doctor.name[0].text);
+        console.log("Facilities: ")
+        doctor.facility.map((fac) => {
+            console.log(fac.name);
+        })
+    }
+})
+
+// Task B
+app.post('/insertCsv', upload.single('file'), (req, res) => {
+    const csvFilePath = req.file.path;
+    csvtojson()
+        .fromFile(csvFilePath)
+        .then((jsonObj) => {
+
+            let id = '';
+            let name = '';
+
+            jsonObj.map((practitioner, index) => {
+                if (id !== practitioner.ID) {
+                    id = practitioner.ID;
+                    name = practitioner.FamilyName;
+                } else if (name !== practitioner.FamilyName) {
+                    return res.status(404).send({ message: "Name is not corresponding with the given ID" });
+                } 
+                console.log(practitioner.FamilyName + ": " + practitioner.NameId);
+            })
+            
+        })
 });
+
+// Task C
+
+
 
 app.listen(9999);
